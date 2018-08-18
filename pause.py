5811122c71861efbf53333e5974b4dd7f0a43595
Tony Holroyd <tony.j.holroyd@gmail.com>
2018-08-18 22:48:14 +0100

    tweek pause

--- pause.py
+++ pause.py
@@ -1,12 +1,11 @@
-class Pause:
+import time
+
+now = time.ticks_ms()
+
+
+def pause(time_to_pause=100):
+    global now
     import time
-
-    __now = time.ticks_ms()
-
-    @classmethod
-    def pause(cls, time_to_pause=100):
-        import time
-        import machine
-        while time.ticks_diff(time.ticks_ms(), Pause.__now) < time_to_pause:
-            pass
-        Pause.__now = time.ticks_ms()
+    while time.ticks_diff(time.ticks_ms(), now) < time_to_pause:
+        pass
+    now = time.ticks_ms()
