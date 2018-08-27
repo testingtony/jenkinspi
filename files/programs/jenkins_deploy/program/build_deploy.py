ba9a39a009a1a268c02c19e773a9d62f42813456
Tony Holroyd <tony.j.holroyd@gmail.com>
2018-08-27 19:33:59 +0100

    add job to merge build and deploy data

---
+++ files/programs/jenkins_deploy/program/build_deploy.py
@@ -0,0 +1,43 @@
+import json
+
+results = {
+    'SUCCESS': 50,
+    'UNSTABLE': 30,
+    'ABORTED': 10,
+    'FAILURE': 10
+}
+
+
+class BuildDeploy:
+    def __init__(self, config):
+        self._config = config
+        self._build_status = None
+        self._deploy_status = None
+        self._first_id = None
+
+    def on_build_message(self, client, message):
+        print("build_message {} {!r}".format(self, message))
+        self._build_status = json.loads(message)
+
+    def on_deploy_message(self, client, message):
+        print("deploy_message {} {!r}".format(self, message))
+        self._deploy_status = json.loads(message)
+
+        if self._build_status is not None:
+            build = self._build_status
+            deploy = self._deploy_status
+
+            publish = dict()
+
+            build_result = build['result']
+            deploy_result = deploy['result']
+            publish['result'] = deploy_result if results[build_result] > results[deploy_result] else build_result
+
+            publish['building'] = build['building'] or deploy['building']
+
+            publish['build_id'] = build['buildid']
+            publish['time'] = deploy['time']
+            publish['timestamp'] = deploy['timestamp']
+
+            if build['timestamp'] < deploy['timestamp']:
+                client.publish(self._config['publish'], json.dumps(publish), qos=1, retain=True)
