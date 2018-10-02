193752d0d99304f067ebf276d1cfc3903d57ad44
Tony Holroyd <tony.j.holroyd@gmail.com>
2018-10-02 20:12:38 +0100

    Use UDP broadcast as alternative way to get configuration

--- files/programs/board_config/program/board_config.py
+++ files/programs/board_config/program/board_config.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import yaml
 import json
+import socket
 
 
 def on_connect(client, userdata, flags, rc):
@@ -9,16 +10,25 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
+    print("Message {!r}".format(msg))
     client_id = msg.topic.split('/')[2]
-    try:
-        with open("../config/micro_{}.yml".format(client_id)) as f:
-            obj = yaml.load(f)
-        js = json.dumps(obj)
+    config = get_config(client_id)
+    if config is not None:
         topic = 'config/reply/{}'.format(client_id)
-        client.publish(topic, js)
-        print("sending a config")
+        client.publish(topic, config)
+
+
+def get_config(client_id):
+    try:
+        file = "../config/micro_{}.yml".format(client_id)
+        with open(file) as f:
+            obj = yaml.load(f)
+            print("Serving config file {}".format(file))
+        js = json.dumps(obj)
+        return js
     except FileNotFoundError as err:
         print("No config file {}".format(err.filename))
+        return None
 
 
 def main(yaml_file="../config/board_config.yml"):
@@ -34,10 +44,27 @@ def main(yaml_file="../config/board_config.yml"):
 
     client.connect(config.get('mq', 'localhost'), config.get('mq_port', 1883), 60)
 
+    udp_listen = config.get('broadcast_address', "0.0.0.0")
+    udp_port = int(config.get('broadcast_port', '5005'))
+
+    sock = socket.socket(socket.AF_INET,  # Internet
+                         socket.SOCK_DGRAM)  # UDP
+    sock.bind((udp_listen, udp_port))
+
     try:
-        client.loop_forever()
+        client.loop_start()
+        while True:
+            try:
+                data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+                config = get_config(str(data, 'utf-8'))
+                if config is not None:
+                    sock.sendto(bytes(config, 'utf-8'), (addr[0], udp_port))
+            except socket.timeout:
+                pass
     finally:
+        client.loop_stop()
         client.disconnect()
+        sock.close()
 
 
 if __name__ == '__main__':
